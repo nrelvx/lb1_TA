1d9935338bb2633060795de54bdacc053c482062
nrelvx <sveronikasveronika01@gmail.com>
2025-10-04 11:33:34 +0300

    Update лб1.py

--- лб1.py
+++ лб1.py
@@ -1,14 +1,21 @@
 import math
 
+R1 = float(input("Введіть внутрішній радіус: "))
+R2 = float(input("Введіть зовнішній радіус: "))
+
+
 def ring(R1, R2):
-    if R1<0 or R2<=R1:
-        print("Помилка, введіть інше значення")
+    if R1 < 0 or R2 <= R1:
+        print("Задача не має розв'язку при R1<0, R2<=R1")
         return None
+    # Обчислення площі кільця
     S = math.pi * (R2**2 - R1**2)
     return S
-R1=float(input("Введіть внутрішній радіус: "))
-R2=float(input("Введіть зовнішній радіус: "))
 
-result=ring(R1, R2)
+
+# Вивід результату
+result = ring(R1, R2)
 if result is not None:
     print(f"Площа кільця S = {result}")
+
+
